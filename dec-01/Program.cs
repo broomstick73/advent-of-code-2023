860c8e7b57af78ed8b1803b83c96bc9a4e8a1fbc
James Conley <james.conley@fanduel.com>
2023-12-06 08:33:34 -0500

    simple shell

--- dec-01/Program.cs
+++ dec-01/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+namespace scl;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var rootCommand = new RootCommand("Sample app for System.CommandLine");
+
+        var inputFileOption = new Option<FileInfo?>(
+            name: "--input",
+            description: "The file to read and display on the console.");
+        rootCommand.AddOption(inputFileOption);
+
+        var outputFileOption = new Option<FileInfo?>(
+            name: "--output",
+            description: "The file to read and display on the console.");
+        rootCommand.AddOption(outputFileOption);
+
+        rootCommand.SetHandler((input, output) =>
+        {
+            ReadFile(input, output);
+        }, inputFileOption, outputFileOption);
+
+        // rootCommand.SetHandler((inut) =>
+        //     {
+        //         ReadFile(inut!);
+        //     },
+        //     fileOption);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    static void ReadFile(FileInfo input, FileInfo output)
+    {
+        File.ReadLines(input.FullName).ToList()
+            .ForEach(line => Console.WriteLine(line));
+    }
+}
---
+++ dec-01/README.md
@@ -0,0 +1,5 @@
+## Run test program
+to run the test program in the terminal run
+```sh
+dotnet run --input test-input.txt --output test-output.txt
+```
